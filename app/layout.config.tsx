984def8f7117fc4647d3452d54ccc005d77d1ce2
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-04-23 12:49:22 +0530

    fix: update header styles

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,6 +1,7 @@
 import type {BaseLayoutProps} from 'fumadocs-ui/layouts/shared';
 import Image from 'next/image';
 import {ThemeToggle} from "fumadocs-ui/components/layout/theme-toggle";
+import Link from 'next/link';
 
 /**
  * Shared layout configurations
@@ -15,16 +16,34 @@ export const baseOptions: BaseLayoutProps = {
         enabled: true
     },
     nav: {
+        url: "https://www.nocodb.com",
         title: (
-            <div className="flex items-center gap-3">
-                <Image src="/img/nocodb-full-color.png" alt="NocoDB" width={120} height={40}/>
+            <div className="flex hidden md:flex items-center gap-3">
+                <Link href="https://www.nocodb.com">
+                    <Image src="/img/nocodb-full-color.png" alt="NocoDB" width={120} height={40}/>
+
+                </Link>
                 <div className="w-[1px] py-2.5 border-1 border-r-0 border-nc-border-grey-medium"></div>
                 <div className="font-semibold text-nc-content-grey-subtle-2 text-[14px] leading-5">
                     Docs
                 </div>
-                <div className="md:flex-1" />
+                <div className="md:flex-1"/>
                 <ThemeToggle/>
             </div>
         ),
+        children: (
+            <div className="flex md:hidden items-center gap-3">
+                <Link href="https://www.nocodb.com">
+                    <Image src="/img/nocodb-full-color.png" alt="NocoDB" width={120} height={40}/>
+
+                </Link>
+                <div className="w-[1px] py-2.5 border-1 border-r-0 border-nc-border-grey-medium"></div>
+                <div className="font-semibold text-nc-content-grey-subtle-2 text-[14px] leading-5">
+                    Docs
+                </div>
+                <div className="md:flex-1"/>
+                <ThemeToggle/>
+            </div>
+        )
     }
 };
