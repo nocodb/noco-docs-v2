3b618b1c1c00bf8eea1c6bf660dea2bbbea39476
Pranav C <pranavxc@gmail.com>
2025-06-07 23:20:36 +0530

    feat: exclude assets file, error pages, and other irrelevant files from sitemap file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,53 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://nocodb.com',
   generateRobotsTxt: true,
   sitemapBaseFileName: 'sitemap-docs',
-  generateIndexSitemap: false
+  generateIndexSitemap: false,
+  exclude: [
+    // Static assets
+    '*.js',
+    '*.css',
+    '*.xml',
+    '*.less',
+    '*.png',
+    '*.jpg',
+    '*.jpeg',
+    '*.gif',
+    '*.pdf',
+    '*.doc',
+    '*.txt',
+    '*.ico',
+    '*.svg',
+    '*.woff',
+    '*.woff2',
+    '*.ttf',
+    '*.eot',
+    // Admin/internal pages
+    '/_next/*',
+    '/_vercel/*',
+    // Error pages
+    '/404',
+    '/500',
+    '/error',
+    // Other exclusions
+    '/sitemap*.xml',
+    '/robots.txt',
+    '/favicon.ico',
+    '/manifest.json',
+    // Development/testing paths
+    '/.well-known/*'
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/*',
+          '/_next/*',
+          '/_vercel/*',
+          '/private/*'
+        ]
+      }
+    ]
+  }
 }
