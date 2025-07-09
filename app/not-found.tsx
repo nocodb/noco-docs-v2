c57959966ad7c690cbb0133ea4eee14d747cd999
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-07-09 13:15:21 +0530

    feat: 404 page error

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Footer } from '@/components/blog/home/Footer'
+import {Navbar} from '@/components/blog/home/Navbar'
+import NotFoundPage from '@/components/NotFoundPage'
+
+export default function NotFound() {
+    return (
+        <>
+        <Navbar />
+        <div className="mt-20" />
+        <NotFoundPage url="/" label="Go to Home" />
+        <Footer/>
+        </>
+
+    )
+}
