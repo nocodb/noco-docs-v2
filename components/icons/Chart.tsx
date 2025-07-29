353bb591f9b3928f3dbc50c614d50060f5ac60f8
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-07-29 22:54:46 +0530

    feat: add chart icon and update dashboard page icons

---
+++ components/icons/Chart.tsx
@@ -0,0 +1,8 @@
+import { ChartNoAxesColumn } from "lucide-react";
+import * as React from "react"
+import {SVGProps} from "react"
+
+const Chart = (props: SVGProps<SVGSVGElement>) => (
+    <ChartNoAxesColumn {...props} />
+)
+export default Chart
